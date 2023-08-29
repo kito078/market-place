97435f473a414cc1e6b1b98269844b3978a40772
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-29 21:45:43 +0200

    d

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="forgot-password/" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
