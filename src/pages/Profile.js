1db516e05be9ee1eae38dd396a74eb742d6e7e54
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-29 21:25:34 +0200

    s

---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Profile!</h1>
+    </div>
+  );
+}
+
+export default Profile;
---
+++ src/pages/SignIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignIn() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">SignIn!</h1>
+    </div>
+  );
+}
+
+export default SignIn;
