fa345da92c53d811f4db5ceb87c05325affced42
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-29 21:22:50 +0200

    s

---
+++ src/pages/Explore.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Explore() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Explore!</h1>
+    </div>
+  );
+}
+
+export default Explore;
