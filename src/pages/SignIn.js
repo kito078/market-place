468f39e0cb764b8dc015cfe95e06ecffb21c921e
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-30 08:31:39 +0200

    s

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../../src/assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../../src/assets/svg/visibilityIcon.svg";
 
 function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
   return (
     <div>
       <h1 class="text-3xl font-bold underline">SignIn!</h1>
