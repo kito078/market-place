50d13d947ba25616be397b54c498995046b056ce
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-04 12:38:51 +0200

    a

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
 
 function Profile() {
-  const [user, setUser] = useState(null);
-
   const auth = getAuth();
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
-  return user ? <h1>{user.displayName}</h1> : "Not Logged In";
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  return <></>;
 }
 
 export default Profile;
