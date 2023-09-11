df52962e68a7d539029306ed5c19c8714b04c198
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-11 23:41:17 +0200

    s

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,9 +1,27 @@
 import React from "react";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 function Explore() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Explore!</h1>
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+
+      <main>
+        {/* slider */}
+        <p className="exploreCategoryHeading">Category</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+          </Link>
+        </div>
+      </main>
     </div>
   );
 }
