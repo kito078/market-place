8763f6e9379e9f6ade5d0f8852279dde59edbaaf
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-11 23:07:01 +0200

    dd

---
+++ src/hooks/UseAuthStatus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UseAuthStatus() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UseAuthStatus
--- src/pages/components/PrivateRoute.js
+++ src/pages/components/PrivateRoute.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { UseAuthStatus } from "../../hooks/UseAuthStatus";
 
 function PrivateRoute() {
-  const loggedIn = false;
+  const { loggedIn, checkingStatus } = UseAuthStatus();
+
+  if (checkingStatus) {
+    return <h3>Loading</h3>;
+  }
+
+  // const loggedIn = false;
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 }
 
