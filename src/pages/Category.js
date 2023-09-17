9e2031ff9b59f04cb986894f3986a9583477e2cd
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-17 05:43:38 +0200

    d

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,5 +1,13 @@
 import React from "react";
-import {collection,getDocs, query, where, order}
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
 
 function Category() {
   return <div>category</div>;
