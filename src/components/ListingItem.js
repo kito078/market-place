5948a8705f375762759297b8417e3646c0a140cd
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-18 10:55:16 +0200

    b

---
+++ src/components/ListingItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
+import bedIcon from "../assets/svg/badgeIcon.svg";
+import bathtubIcon from "../assets/svg/bathtubIcon.svg";
+
+function ListingItem({ listing, id }) {
+  return (
+    <li className="categoryListing">
+      <Link
+        to={`/category/${listing.type}/${id}`}
+        className="categoryListingLink"
+      >
+        <img src={listing.imgUrls[0]} alt="" className="categoryListingImg" />
+      </Link>
+    </li>
+  );
+}
+
+export default ListingItem;
